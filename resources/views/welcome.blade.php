7cbaa37f712f879563696372b6214724b3ccb324
toyoda <tytmixx@gmail.com>
2022-01-24 02:14:04 +0000

    modify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,8 @@
     @if (Auth::check())
         {{ Auth::user()->name }}
     @else
-        <div class="center jumbotron">
-            <div class="text-center">
-                <h1>Welcome to the Microposts</h1>
-                {{-- ユーザ登録ページへのリンク --}}
-                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
-            </div>
+        <div class="text-center">
+            <h1>Welcome!!</h1>
         </div>
     @endif
 @endsection
